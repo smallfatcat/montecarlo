96ed5f643505c71128ffba8d8988d6b309350a41
smallfatcat <david.imrie@gmail.com>
2025-08-16 11:59:46 +0100

    before merge

---
+++ src/version.h
@@ -0,0 +1,15 @@
+// Auto-generated version header
+// Generated on: 2025-08-15T03:29:06Z
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_BUILD 53
+#define VERSION_COMMIT "b17f999"
+#define VERSION_BRANCH "multiplay"
+#define VERSION_STRING "0.0.0-build.53.dirty"
+
+#endif // VERSION_H
