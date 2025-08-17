d6eedae694a76d3ba27b944d5297615afb23ca64
smallfatcat <david.imrie@gmail.com>
2025-08-17 03:14:38 +0100

    actual version

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 // Auto-generated version header
-// Generated on: 2025-08-17T02:13:37Z
+// Generated on: 2025-08-17T02:14:27Z
 
 #ifndef VERSION_H
 #define VERSION_H
@@ -7,9 +7,9 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 81
-#define VERSION_COMMIT "9678a6c"
+#define VERSION_BUILD 82
+#define VERSION_COMMIT "5741478"
 #define VERSION_BRANCH "main"
-#define VERSION_STRING "0.0.0-build.81.dirty"
+#define VERSION_STRING "0.0.0-build.82"
 
 #endif // VERSION_H
