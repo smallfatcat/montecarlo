d21a3eeaf73342ea9dcc16747eb5434699764cba
smallfatcat <david.imrie@gmail.com>
2025-08-18 01:07:32 +0100

    small commit

--- sidepot_demo.js
+++ sidepot_demo.js
@@ -26,9 +26,11 @@ class Pots {
             sidepot.level = newLevel
             this.sidepots.push(sidepot)
         }
+        info("Pots", "Created " + this.sidepots.length + " sidepots")
     }
 
     addBet(player, amount) {
+        info("addBet", "Player " + player + " -" + amount)
         for (let i = 0; i < this.sidepots.length; i++) {
             let level = this.sidepots[i].level
             let allowedBetForLevel = Math.min(amount, level)
@@ -54,96 +56,123 @@ class Pots {
 
 }
 
-const allIn = (playerData, pots, player, amount) => {
-    playerData[player].allIn = true
-    playerData[player].stack = amount
+const allIn = (playerData, pots, player) => {
+    playerData[player].isAllIn = true
+    amount = playerData[player].stack
     pots.addBet(player, amount)
+    playerData[player].stack = playerData[player].stack - amount
 }
 
-const getWinners = (handStrengths, playersEligibleForPot) => {
+const getWinners = (playerData, handStrengths, playersEligibleForPot) => {
     let winners = []
     let maxHandStrength = 0
     for (let player in handStrengths) {
         if (playersEligibleForPot.includes(player)) {
-            if (handStrengths[player] > maxHandStrength) {
+            if (handStrengths[player] > maxHandStrength && !playerData[player].isFolded) {
                 maxHandStrength = handStrengths[player]
             }
         }
     }
     playersEligibleForPot.map(player => {
-        if (handStrengths[player] === maxHandStrength) {
+        if (handStrengths[player] === maxHandStrength && !playerData[player].isFolded) {
             winners.push(player)
         }
     })
     return winners
 }
 
-let playerData = {
-    'a': {
-        'stack': 10,
-        'handStrength': 100,
-        'folded': false,
-        'allIn': false,
-    },
-    'b': {
-        'stack': 100,
-        'handStrength': 90,
-        'folded': false,
-        'allIn': false,
-    },
-    'c': {
-        'stack': 100,
-        'handStrength': 80,
-        'folded': false,
-        'allIn': false,
-    },
-    'd': {
-        'stack': 200,
-        'handStrength': 70,
-        'folded': false,
-        'allIn': false,
-    },
-    'e': {
-        'stack': 200,
-        'handStrength': 70,
-        'folded': false,
-        'allIn': false,
-    },
-    'f': {
-        'stack': 500,
-        'handStrength': 50,
-        'folded': false,
-        'allIn': false,
+const calculateSidepotWinners = (playerData, pots) => {
+    let handStrengths = {}
+    for (let player in playerData) {
+        handStrengths[player] = playerData[player].handStrength
+    }
+
+    for (let i = 0; i < pots.sidepots.length; i++) {
+        let winners = getWinners(playerData, handStrengths, pots.playersEligibleForPot(i))
+        let payout = pots.getPotTotal(i) / winners.length
+        winners.map(winner => {
+            playerData[winner].stack = playerData[winner].stack + payout
+            info("calculateSidepotWinners", "Player " + winner + " +" + payout)
+        })
     }
 }
 
-let handStrengths = {}
-for (let player in playerData) {
-    handStrengths[player] = playerData[player].handStrength
+class Player {
+    constructor(stack, handStrength) {
+        this.stack = stack
+        this.handStrength = handStrength
+        this.isFolded = false
+        this.isAllIn = false
+        info("Player", "Created player with stack " + stack + " and hand strength " + handStrength)
+    }
+    fold() {
+        this.isFolded = true
+    }
+    allIn() {
+        this.isAllIn = true
+    }
 }
 
-const pots = new Pots(playerData)
+const runAllInTest = (playerData) => {
+    // Create pots
+    const pots = new Pots(playerData)
 
-allIn(playerData, pots, 'a', 10)
-allIn(playerData, pots, 'b', 100)
-allIn(playerData, pots, 'c', 100)
-allIn(playerData, pots, 'd', 200)
-allIn(playerData, pots, 'e', 200)
-allIn(playerData, pots, 'f', 500)
+    for (let player in playerData) {
+        info("beforeAllIn", player + " " + playerData[player].stack)
+    }
 
-console.log('Final sidepots state:')
-console.log(pots)
+    // All in every player, mutates playerData and pots
+    for (let player in playerData) {
+        allIn(playerData, pots, player)
+    }
 
-for (let i = 0; i < pots.sidepots.length; i++) {
-    console.log(pots.playersEligibleForPot(i), pots.getPotTotal(i))
+    // Calculate sidepot winners, mutates playerData
+    calculateSidepotWinners(playerData, pots)
+
+    for (let player in playerData) {
+        info("afterAllIn", player + " " + playerData[player].stack)
+    }
 }
 
-for (let i = 0; i < pots.sidepots.length; i++) {
-    console.log(getWinners(handStrengths, pots.playersEligibleForPot(i)))
+const info = (type, message) => {
+    timestamp = new Date().toISOString()
+    if (config.debug[type]) {
+        console.log(timestamp + " " + type + ": " + message)
+    }
+    logData.push({ timestamp, type, message })
 }
 
-console.log(playerData)
+const logData = []
 
+const config = {
+    debug: {
+        addBet: true,
+        calculateSidepotWinners: true,
+        beforeAllIn: true,
+        afterAllIn: true,
+        Pots: true,
+        Player: true,
+        logData: false,
+    }
+}
+
+const main = () => {
+    let playerData = {
+        a: new Player(10, 100),
+        b: new Player(100, 90),
+        c: new Player(100, 90),
+        d: new Player(200, 70),
+        e: new Player(200, 60),
+        f: new Player(500, 50),
+    }
+
+    runAllInTest(playerData)
+    if (config.debug.logData) {
+        console.log(logData)
+    }
+}
+
+main()
 
 
 
