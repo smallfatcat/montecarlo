9a860b0619f7bbd8a62cde18d03695100894aa0b
smallfatcat <david.imrie@gmail.com>
2025-08-19 04:52:18 +0100

    deployment build

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 // Auto-generated version header
-// Generated on: 2025-08-19T02:23:43Z
+// Generated on: 2025-08-19T03:43:18Z
 
 #ifndef VERSION_H
 #define VERSION_H
@@ -7,9 +7,9 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 99
-#define VERSION_COMMIT "435e1e5"
+#define VERSION_BUILD 102
+#define VERSION_COMMIT "710ffa1"
 #define VERSION_BRANCH "convex"
-#define VERSION_STRING "0.0.0-build.99.dirty"
+#define VERSION_STRING "0.0.0-build.102"
 
 #endif // VERSION_H
