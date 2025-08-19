e902b0f42b47e9ea6f42486b6708c4f26f15bbb9
smallfatcat <david.imrie@gmail.com>
2025-08-19 20:06:06 +0100

    build

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 // Auto-generated version header
-// Generated on: 2025-08-19T11:02:26Z
+// Generated on: 2025-08-19T11:30:28Z
 
 #ifndef VERSION_H
 #define VERSION_H
@@ -7,9 +7,9 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 115
-#define VERSION_COMMIT "417745e"
+#define VERSION_BUILD 116
+#define VERSION_COMMIT "e4738a6"
 #define VERSION_BRANCH "convex"
-#define VERSION_STRING "0.0.0-build.115.dirty"
+#define VERSION_STRING "0.0.0-build.116"
 
 #endif // VERSION_H
